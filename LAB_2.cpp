1c917ec0e07ca6c0fc9e553e4d3e7b3ffce94614
Your Name <you@example.com>
2021-10-05 14:30:10 +0700

    C-1

---
+++ LAB_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+std::cout << "I like Information Technology. \n" ;
+std::cout << "I want to Programmer." << endl;
+return 0;
+}
---
+++ LAB_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "Student Code : 6406021420344" << endl;
+cout << "Student Name : tada somnuk" << endl;
+cout << "nickname : Film" << endl;
+cout << "Division : Information Technologyk" << endl;
+cout << "Year : 1" << endl;
+cout << "Room : RA" << endl;
+cout << "Sex : Male" << endl;
+cout << "Age : 20" << endl;
+cout << "Address : 59 Tharua-Praputtabant Tharua Tharua Ayutthaya 13130 Thailand" << endl;
+cout << "Phone number : 096-752-0686" << endl;
+
+return (0);
+}
